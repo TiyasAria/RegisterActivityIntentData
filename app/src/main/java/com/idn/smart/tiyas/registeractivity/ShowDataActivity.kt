e2b595e4f1bc902df359f99e1a041b4552d45609
TiyasAria <tiyasaria123@gmail.com>
2020-12-06 17:48:56 +0700

    melakukan perubahan pada variabel karena, variablenya saya delete, karena kotlin bisa memanggil berdasarkan ID.

--- app/src/main/java/com/idn/smart/tiyas/registeractivity/ShowDataActivity.kt
+++ app/src/main/java/com/idn/smart/tiyas/registeractivity/ShowDataActivity.kt
@@ -8,19 +8,9 @@ import kotlinx.android.synthetic.main.activity_show_data.*
 
 class ShowDataActivity : AppCompatActivity() {
 
-    private lateinit var tvInputUsername : TextView
-    private lateinit var tvInputEmail : TextView
-    private lateinit var tvInputPassword : TextView
-    private lateinit var tvInputConfirmPassword : TextView
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_data)
-
-        tvInputUsername = tv_inputUsername
-        tvInputEmail = tv_inputEmail
-        tvInputPassword = tv_inputPassword
-        tvInputConfirmPassword = tv_inputresetpwd
 
         val intentTerima = intent
         val terimaUsername = intentTerima.getStringExtra("amplopUsername")
@@ -28,9 +18,9 @@ class ShowDataActivity : AppCompatActivity() {
         val terimaPassword = intentTerima.getStringExtra("amplopPassword")
         val terimaConfirmPassowrd = intentTerima.getStringExtra("amplopConfirmPassword")
 
-        tvInputUsername.text = terimaUsername
-        tvInputEmail.text = terimaEmail
-        tvInputPassword.text = terimaPassword
-        tvInputConfirmPassword.text = terimaConfirmPassowrd
+        tv_inputUsername.text = terimaUsername
+        tv_inputEmail.text = terimaEmail
+        tv_inputPassword.text = terimaPassword
+        tv_inputresetpwd.text = terimaConfirmPassowrd
     }
 }
--- app/src/main/java/com/idn/smart/tiyas/registeractivity/SignUpActivity.kt
+++ app/src/main/java/com/idn/smart/tiyas/registeractivity/SignUpActivity.kt
@@ -4,55 +4,43 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity(), View.OnClickListener {
-    private lateinit var btnDaftar: Button
-    private lateinit var etUsername: EditText
-    private lateinit var etEmail: EditText
-    private lateinit var etPassword: EditText
-    private lateinit var etConfirmPassword: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
-        etUsername = et_username
-        etEmail = et_email
-        etPassword = et_password
-        etConfirmPassword = et_confirmPassword
-        btnDaftar = btn_daftar
-       btnDaftar.setOnClickListener(this)
+       btn_daftar.setOnClickListener(this)
 
     }
 
     override fun onClick(p0: View) {
-        val inputUsername = etUsername.text.toString().trim()
-        val inputEmail = etEmail.text.toString().trim()
-        val inputPassword = etPassword.text.toString().trim()
-        val inputConfimPassword = etConfirmPassword.text.toString().trim()
+        val inputUsername = et_username.text.toString().trim()
+        val inputEmail = et_email.text.toString().trim()
+        val inputPassword = et_password.text.toString().trim()
+        val inputConfimPassword = et_confirmPassword.text.toString().trim()
 
         var isEmptyField = false
         //membuat variable jika kosong
         when {
             inputUsername.isEmpty() -> {
                 isEmptyField = true
-                etUsername.error = getString(R.string.txt_error)
+                et_username.error = getString(R.string.txt_error)
             }
             //
             inputEmail.isEmpty() -> {
                 isEmptyField = true
-                etEmail.error = getString(R.string.txt_error)
+                et_email.error = getString(R.string.txt_error)
             }
             inputPassword.isEmpty() -> {
                 isEmptyField = true
-                etPassword.error = getString(R.string.txt_error)
+                et_password.error = getString(R.string.txt_error)
             }
 
             inputConfimPassword.isEmpty() -> {
                 isEmptyField = true
-                etConfirmPassword.error = getString(R.string.txt_error)
+                et_confirmPassword.error = getString(R.string.txt_error)
             }
         }
         if (!isEmptyField){
