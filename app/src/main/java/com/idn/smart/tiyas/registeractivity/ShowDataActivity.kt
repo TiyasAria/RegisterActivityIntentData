42fa2cdaf2c575c5097e00fad77c765dd27de2a5
TiyasAria <tiyasaria123@gmail.com>
2020-12-06 22:24:20 +0700

    melakukan perubahan pada variabel karena, variablenya saya delete, karena kotlin bisa memanggil berdasarkan ID.

--- app/src/main/java/com/idn/smart/tiyas/registeractivity/ShowDataActivity.kt
+++ app/src/main/java/com/idn/smart/tiyas/registeractivity/ShowDataActivity.kt
@@ -2,7 +2,6 @@ package com.idn.smart.tiyas.registeractivity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_show_data.*
 
 
